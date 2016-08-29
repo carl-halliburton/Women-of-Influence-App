12f1fd6f3fe1e0c77b3423ee3014ba20806e83b3
nicholasrowley <nick-1.03@hotmail.com>
2016-08-30 10:35:42 +1200

    Implemented full screen button
    - Video view webview goes to full screen however you cannot get out of fullscreen due to a lack of a fullscreen button while in full screen.

--- app/src/main/java/com/example/carl/womenofinfluence/VideoView.java
+++ app/src/main/java/com/example/carl/womenofinfluence/VideoView.java
@@ -12,13 +12,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.webkit.WebView;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class VideoView extends AppCompatActivity {
 
     private Singleton tempSingleton;
     private WebView myWebview;
+    private Button fullScreenButton;
+    private TextView titleText;
+    private TextView aboutText;
+    private Toolbar screenToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +37,12 @@ public class VideoView extends AppCompatActivity {
         toolbar.setNavigationIcon(R.mipmap.ic_home_white);
 
         tempSingleton = Singleton.getInstance();
+
+        //map the widgets to objects
+        fullScreenButton = (Button) findViewById(R.id.fullScreenButton);
+        titleText = (TextView) findViewById(R.id.appTitle);
+        aboutText = (TextView) findViewById(R.id.aboutVidText);
+        screenToolbar = (Toolbar) findViewById(R.id.toolbar);
 
         //implementation of WebView for the video
         myWebview = (WebView) findViewById(R.id.webView);
@@ -79,6 +92,10 @@ public class VideoView extends AppCompatActivity {
     //full screen onClick method (currently incomplete)
     public void onClickFullscreen( View v )
     {
+        screenToolbar.setVisibility(View.GONE);
+        fullScreenButton.setVisibility(View.GONE);
+        titleText.setVisibility(View.GONE);
+        aboutText.setVisibility(View.GONE);
         myWebview.setLayoutParams(new LinearLayout.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT));
     }
 }
