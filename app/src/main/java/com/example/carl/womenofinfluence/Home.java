f7050e0347e01280dc24694e2d906beb5204f5fb
Carl <carl.j.halliburton@gmail.com>
2016-09-04 17:39:20 +1200

    video works in fullscreen in landscape, but it crashes if you try to switch between them, forexample the device mus tbe in the orientation you wish to be in before playing.

--- app/src/main/java/com/example/carl/womenofinfluence/Home.java
+++ app/src/main/java/com/example/carl/womenofinfluence/Home.java
@@ -7,9 +7,12 @@ import android.support.v4.app.NavUtils;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.CheckBox;
 
 public class Home extends AppCompatActivity {
