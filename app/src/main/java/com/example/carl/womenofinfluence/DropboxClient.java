481c0b8572d686b5f26a5f85746b342fff8d448d
nick <nick-1.03@hotmail.com>
2016-10-01 14:21:46 +1300

    Updated deprecated dropbox client code.

--- app/src/main/java/com/example/carl/womenofinfluence/DropboxClient.java
+++ app/src/main/java/com/example/carl/womenofinfluence/DropboxClient.java
@@ -3,6 +3,8 @@ package com.example.carl.womenofinfluence;
 import com.dropbox.core.DbxRequestConfig;
 import com.dropbox.core.v2.DbxClientV2;
 
+import static com.dropbox.core.v2.files.FolderMetadata.newBuilder;
+
 /**
  * Created by nick- on 8/09/2016.
  */
@@ -10,8 +12,7 @@ public class DropboxClient {
 
     public static DbxClientV2 getClient(String ACCESS_TOKEN) {
         // Create Dropbox client
-        //TODO Deprecated code here
-        DbxRequestConfig config = new DbxRequestConfig("dropbox/sample-app", "en_US");
+        DbxRequestConfig config = DbxRequestConfig.newBuilder("Pearls/v3.0").build();
         DbxClientV2 client = new DbxClientV2(config, ACCESS_TOKEN);
         return client;
     }
