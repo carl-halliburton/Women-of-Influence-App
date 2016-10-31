b6e9e8d5c73db3f8004ec5d9e5da12864e3f08e5
carlHalliburton <carl.j.halliburton@gmail.com>
2016-10-31 15:41:30 +1300

    added facebook share icon

--- app/src/main/java/nz/co/hawkefilms/womenofinfluence/ShareVideo.java
+++ app/src/main/java/nz/co/hawkefilms/womenofinfluence/ShareVideo.java
@@ -1,8 +1,24 @@
 package nz.co.hawkefilms.womenofinfluence;
 
+import android.content.Intent;
+import android.net.Uri;
+
 /**
  * Created by carl on 31/10/2016.
  */
 
 public class ShareVideo {
-}
+
+    public ShareVideo() {
+
+    }
+
+    public void snedEmailIntent() {
+            Intent intent = new Intent(Intent.ACTION_SENDTO);
+            intent.setType("text/html");
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Subject - Name of Video");
+            intent.putExtra(Intent.EXTRA_TEXT, "You might be interested in this video " + "- PLACE LINK HERE");
+            intent.setData(Uri.parse("mailto:"));
+            //startActivity(Intent.createChooser(intent, "Send Email"));
+        }
+    }
