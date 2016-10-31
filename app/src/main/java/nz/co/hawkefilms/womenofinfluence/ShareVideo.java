8a1d251b7c67a555fad72ff1918140e4442070f8
carlHalliburton <carl.j.halliburton@gmail.com>
2016-10-31 17:36:16 +1300

    added intent code for email and sms to shareVideo class they are not connected yet

--- app/src/main/java/nz/co/hawkefilms/womenofinfluence/ShareVideo.java
+++ app/src/main/java/nz/co/hawkefilms/womenofinfluence/ShareVideo.java
@@ -7,13 +7,13 @@ import android.net.Uri;
  * Created by carl on 31/10/2016.
  */
 
-public class ShareVideo {
+public class ShareVideo
 
     public ShareVideo() {
 
     }
 
-    public void snedEmailIntent() {
+    public void sendEmailIntent() {
             Intent intent = new Intent(Intent.ACTION_SENDTO);
             intent.setType("text/html");
             intent.putExtra(Intent.EXTRA_SUBJECT, "Subject - Name of Video");
@@ -21,4 +21,11 @@ public class ShareVideo {
             intent.setData(Uri.parse("mailto:"));
             //startActivity(Intent.createChooser(intent, "Send Email"));
         }
+
+    public void sendSmsIntent() {
+        Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+        sendIntent.putExtra("sms_body", "default content");
+        sendIntent.setType("vnd.android-dir/mms-sms");
+        //startActivity(sendIntent);
     }
+}
