f397e77826b4222c79400b4f24dd17c72dbb98f1
nick <nick-1.03@hotmail.com>
2016-11-19 16:08:58 +1300

    Added search results code.

---
+++ app/src/main/java/nz/co/hawkefilms/womenofinfluence/SearchResults.java
@@ -0,0 +1,80 @@
+package nz.co.hawkefilms.womenofinfluence;
+
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class SearchResults extends AppCompatActivity {
+
+    private GlobalAppData appData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_results);
+        setTitle(null);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.mipmap.ic_launcher); // inserts home icon on the left of the toolbar
+
+        appData = GlobalAppData.getInstance(getString(R.string.ACCESS_TOKEN), SearchResults.this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_notification:
+                openAppSettings();
+                return true;
+            case R.id.menu_video_gallery:
+                startActivity(new Intent(SearchResults.this, VideoGallery.class));
+                return true;
+            case R.id.menu_feedback:
+                startActivity(new Intent(SearchResults.this, Feedback.class));
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //Opens the app setting so the user can turn notifications on or off
+    public void openAppSettings() {
+        String packageName = getString(R.string.package_name);
+
+        try {
+            //Open the specific App Info page:
+            Intent intent = new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+            intent.setData(Uri.parse("package:" + packageName));
+            startActivity(intent);
+
+        } catch ( ActivityNotFoundException e ) {
+            new AlertDialog.Builder(SearchResults.this)
+                    .setTitle("Notification Settings Not Available")
+                    .setMessage("Unable to open the apps settings screen, please try again later")
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            // continue with delete
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+        }
+    }
+}
